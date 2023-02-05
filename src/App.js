1450eb1b37c432ade00186094d2ceb34c3ba2604
Aditya Uniyal <uniyal2001@gmail.com>
2023-02-05 15:42:37 +0530

    made a calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+//icons
+import BackspaceIcon from '@mui/icons-material/Backspace';
+
 function App() {
+
+  const [count, setCount] = useState('');
+
+  const handleClick = (value) => {
+    setCount(count + value);
+  };
+
+  const handleEvaluate = () => {
+    try {
+      setCount(eval(count).toString());
+    } catch (error) {
+      setCount('Error');
+    }
+  };
+
+  const handleDelete = () => {
+    setCount(count.slice(0, -1));
+  };
+
+  const handleClear = () => {
+    setCount('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='wrapper'>
+        <div className='center__up'>
+          <input type="text" value={count} readOnly />
+        </div>
+        <div className='center__down'>
+          <div className='buttons__inline'>
+            <button onClick={handleClear}>CE</button>
+            <BackspaceIcon className='button' onClick={handleDelete} />
+            <button onClick={() => handleClick('%')}>÷</button>
+            <button onClick={() => handleClick('/')}>/</button>
+          </div>
+          <div className='buttons__inline'>
+            <button onClick={() => handleClick('7')}>7</button>
+            <button onClick={() => handleClick('8')}>8</button>
+            <button onClick={() => handleClick('9')}>9</button>
+            <button onClick={() => handleClick('*')}>x</button>
+          </div>
+          <div className='buttons__inline'>
+            <button onClick={() => handleClick('4')}>4</button>
+            <button onClick={() => handleClick('5')}>5</button>
+            <button onClick={() => handleClick('6')}>6</button>
+            <button onClick={() => handleClick('-')}>-</button>
+          </div>
+          <div className='buttons__inline'>
+            <button onClick={() => handleClick('1')}>1</button>
+            <button onClick={() => handleClick('2')}>2</button>
+            <button onClick={() => handleClick('3')}>3</button>
+            <button onClick={() => handleClick('+')}>+</button>
+          </div>
+          <div className='buttons__inline'>
+            <button onClick={() => handleClick('0')}>0</button>
+            <button onClick={() => handleClick('00')}>00</button>
+            <button onClick={() => handleClick('.')}>.</button>
+            <button onClick={handleEvaluate}>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
